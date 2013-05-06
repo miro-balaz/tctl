db772a19b70d68be55a102d8b939bc8c466fc959
U-HQ\mbalaz <mbalaz@NBMBALAZ.hq.eset.com>
2013-05-06 22:10:03 +0200

    fixed bugs

--- fenwick.cpp
+++ fenwick.cpp
@@ -6,46 +6,52 @@ class FenwickTree {
   int start_value_;
   // inits 0..count
   
-  FenwickTree(int count):this(0, count-1){ 
-  }
-
+  public:
   FenwickTree(int begin, int end){ // classical closed-open interval of allowed values
-    max_value_ = end - begin + 1; //end - begin is the size, and+1 because we do not use zero-th element
-    start_value_ = begin -1;
+    max_value_ = end - begin + 1; //end - begin is the size, and+1 because we do not use zero-th element ==n+2
+    start_value_ = begin -1; //==-1
     c = vector<T>(max_value_, 0);
   }
-
+  // allowed values will be 0 to count-1
+//  FenwickTree(int count):FenwickTree(0, count){ 
+ // }
+  // Returns sum from begin to idx inclusively, if idx isoutside the bounds it will return 0 or sum for max value depending of the idx.
   T get_sum(int idx) {
-    idx-=start_value;
+    idx -= start_value_;
     //assert(idx>=0 && idx<max_value_);
     if (idx<1) return 0;
     if (idx>=max_value_) idx = max_value_ - 1;
-    return get_cummulative_no_check(idx);
+    return get_sum_no_check(idx);
   }
 
-  inline T get_cummulative_no_check(int idx) {
+  // The actual implementation of get_sum
+  inline T get_sum_no_check(int idx) {
+    int rval=0;
     while(idx) {
-      rval+=c[idx];
-      idx -= ixd & (-idx);
+      rval += c[idx];
+      idx -= idx & (-idx);
     }
     return rval;
   }
-
-  T update(int idx, int val) {
-    idx-=start_value;
-    //assert(idx>=0 && idx<max_value_);
-    if (idx < 1) return 0;
-    if (idx >= max_value_) idx = max_value_ - 1;
-    update_no_check(idx, val)
-
-  }
-
-  inline T update_no_check(int idx, int val) {
-    while (idx <= MaxVal){
-      tree[idx] += val;
+  // The actual implementation of add
+  inline void add_no_check(int idx, T val) {
+    while (idx < max_value_){
+      c[idx] += val;
       idx += (idx & -idx);
     }
   }
+
+  // increases the frequency for idx
+  void add(int idx, T val) {
+    idx-=start_value_;
+    //assert(idx>=0 && idx<max_value_);
+    if (idx < 1) return ;
+    if (idx >= max_value_) idx = max_value_ - 1;
+    add_no_check(idx, val);
+
+  }
+
+  // The slower version of getting value for single element
   T get_single(int idx) {
     return get_sum(idx)-get_sum(idx-1);
   }
