9d4f3bcb9bcb36783a8820f0c6f49d3417c41f6d
Miroslav Balaz <miroslav.balaz@eset.sk>
2013-08-26 16:41:49 +0200

    fixed

--- fenwick.cpp
+++ fenwick.cpp
@@ -1,48 +1,52 @@
-template <typename T>
 //=FenwickTree
+template<typename T>
 class FenwickTree {
+    public:
   vector<T> c;
   int max_value_;
   int start_value_;
   // inits 0..count
   
-  FenwickTree(int count):this(0, count-1){ 
+  FenwickTree(int count):FenwickTree(0, count-1){ 
   }
 
-  FenwickTree(int begin, int end){ // classical closed-open interval of allowed values
+  FenwickTree(int begin, int end){ // closed-open interval of allowed values
     max_value_ = end - begin + 1; //end - begin is the size, and+1 because we do not use zero-th element
-    start_value_ = begin -1;
-    c = vector<T>(max_value_, 0);
+    start_value_ = begin -1; // valid entries as 1 .. max_value
+    c = vector<T>(max_value_+1, 0);
   }
 
   T get_sum(int idx) {
-    idx-=start_value;
-    //assert(idx>=0 && idx<max_value_);
+    idx-=start_value_;
     if (idx<1) return 0;
-    if (idx>=max_value_) idx = max_value_ - 1;
+    if (idx>max_value_) idx = max_value_;
+    //assert(idx>=0 && idx<=max_value_);
     return get_cummulative_no_check(idx);
   }
 
   inline T get_cummulative_no_check(int idx) {
-    while(idx) {
+    T rval = 0;
+    while(idx > 0) {
+      //assert(idx<=max_value_);
       rval+=c[idx];
-      idx -= ixd & (-idx);
+      idx -= idx & (-idx);
     }
     return rval;
   }
 
-  T update(int idx, int val) {
-    idx-=start_value;
-    //assert(idx>=0 && idx<max_value_);
-    if (idx < 1) return 0;
-    if (idx >= max_value_) idx = max_value_ - 1;
-    update_no_check(idx, val)
+  void update(int idx, int val) {
+    idx-=start_value_;
+    if (idx < 1) return;
+    if (idx > max_value_) idx = max_value_ ;
+    //assert(idx>=0 && idx<=max_value_);
+    update_no_check(idx, val);
 
   }
 
-  inline T update_no_check(int idx, int val) {
-    while (idx <= MaxVal){
-      tree[idx] += val;
+  inline void update_no_check(int idx, int val) {
+    while (idx <= max_value_){
+      //assert(idx>=0);
+      c[idx] += val;
       idx += (idx & -idx);
     }
   }
